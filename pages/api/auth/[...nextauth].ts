b314eb3b05641bee2de9cb70dd4b928d50546003
baram1 <Maksymilian.Baran@pega.com>
2025-03-12 23:59:42 +0100

    add callback

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -12,7 +12,13 @@ export const authOptions = {
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma), 
+  callbacks: {
+    async session({ session, token }) {
+      if (token?.accessToken) session.accessToken = token.accessToken
+      return session
+    }
+  }, 
 };
 
 export default NextAuth(authOptions);
